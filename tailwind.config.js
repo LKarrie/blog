27f03fa589e9a3063fb75008cd76aec310a98851
lkarrie <lkarrie@163.com>
2023-06-06 00:20:21 +0800

    do homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: ["./layouts/**/*.html"],
   theme: {
     extend: {
+      textShadow: {
+        sm: '0 1px 0 var(--tw-shadow-color)',
+        DEFAULT: '0 4px 0 var(--tw-shadow-color)',
+        lg: '0 8px 0 var(--tw-shadow-color)',
+      },
       backgroundImage: {
         'main': "url('../img/Novelance_68656805_p0.jpg')"
       },
@@ -26,5 +33,16 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
